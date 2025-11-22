455771bce647dc051a6ef54a9b8b5c609fcd5723
MrT-source <tn3952o@gre.ac.uk>
2025-11-22 17:36:56 +0000

    coordinates formating

--- Dobot/lib/Coordinates/coordinates.cpp
+++ Dobot/lib/Coordinates/coordinates.cpp
@@ -3,27 +3,49 @@
 #include <Frame_h.h>
 #include <avr/io.h>
 
+//coordinates grid defined by visio drawings and measurements
+float highZ = 80.00f;
+float lowZ1 = -50.00f;
+float lowZ2 = -20.00f;
+float lowZ3 = 10.00f;
+
+float lx = 20.00f;
+float ly = -278.00f;
+
+float rrx = 232.00f;
+float rry = -70.00f;
+
+float rx = 76.00f;
+float ry = -226.00f;
+
+float gx = 129.00f;
+float gy = 174.00f;
+
+float bx = 180.00f;
+float by = -122.00f;
+
+
 const Point points[POINT_COUNT] = {
-    [POINT_retrievalLow] = {233.16f, -69.85f, -54.63f, -44.88f},
-    [POINT_retrievalHigh] = {234.18f, -68.39f, 27.84f, -44.48f},
+    [POINT_retrievalLow] = {rrx, rry, lowZ1, -44.88f},
+    [POINT_retrievalHigh] = {rrx, rry, highZ, -44.48f},
     
-    [POINT_blueStorageHigh] = {185.13f, -136.87f, 65.37f, -64.68f},
-    [POINT_blueStorageSpace1] = {180.82f, -125.47f, -53.28f, -62.96f},
-    [POINT_blueStorageSpace2] = {180.82f, -125.47f, -22.28f, -62.96f},
-    [POINT_blueStorageSpace3] = {180.82f, -125.47f, 10.00f, -64.68f},
+    [POINT_blueStorageHigh] = {bx, by, highZ, -64.68f},
+    [POINT_blueStorageSpace1] = {bx, by, lowZ1, -62.96f},
+    [POINT_blueStorageSpace2] = {bx, by, lowZ2, -62.96f},
+    [POINT_blueStorageSpace3] = {bx, by, lowZ3, -64.68f},
 
-    [POINT_greenStorageHigh] = {130.25f, -176.69f, 81.42f, -81.14f},
-    [POINT_greenStorageSpace1] = {130.25f, -176.69f, -54.12f, -81.80f},
-    [POINT_greenStorageSpace2] = {130.25f, -176.69f, -22.12f, -81.80f},
-    [POINT_greenStorageSpace3] = {130.25f, -176.69f, 10.00f, -81.80f},
+    [POINT_greenStorageHigh] = {gx, gy, highZ, -81.14f},
+    [POINT_greenStorageSpace1] = {gx, gy, lowZ1, -81.80f},
+    [POINT_greenStorageSpace2] = {gx, gy, lowZ2, -81.80f},
+    [POINT_greenStorageSpace3] = {gx, gy, lowZ3, -81.80f},
 
-    [POINT_redStorageHigh] = {72.60f, -259.71f, 67.63f, -102.58f},
-    [POINT_redStorageSpace1] = {76.73f, -228.93f, -54.06f, -99.67f},
-    [POINT_redStorageSpace2] = {82.73f, -228.93f, -22.06f, -99.67f},
-    [POINT_redStorageSpace3] = {85.73f, -228.93f, 10.00f, -99.67f},
+    [POINT_redStorageHigh] = {rx, ry, highZ, -102.58f},
+    [POINT_redStorageSpace1] = {rx, ry, lowZ1, -99.67f},
+    [POINT_redStorageSpace2] = {rx, ry, lowZ2, -99.67f},
+    [POINT_redStorageSpace3] = {rx, ry, lowZ3, -99.67f},
 
-    [POINT_pickUpHigh] = {13.63f, -294.88f, 46.83f, -115.56f},
-    [POINT_pickUpLow] = {20.65f, -278.84f, -51.08f, -113.97f},
+    [POINT_pickUpHigh] = {lx, ly, highZ, -115.56f},
+    [POINT_pickUpLow] = {lx, ly, lowZ1, -113.97f},
 };
 
 void moveToPose(uint8_t mode, PointId id)
